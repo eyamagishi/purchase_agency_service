dd289e179d05d5a04eeb7b2146046168f8acf7cf
eyamagishi <e.yamagishi0625@gmail.com>
2024-06-15 21:42:43 +0900

    add: Vueコンポーネントを追加

--- app/javascript/App.vue
+++ app/javascript/App.vue
@@ -1,27 +1,23 @@
 <template>
-    <header>
-        <h1 id="logo"><a href="#">{{ state.message }}</a></h1>
-    </header>
+    <Header></Header>
+    <nav id="menubar">
+        <ul>
+            <li><a href="#">HOME</a></li>
+            <li><a href="#">COMPANY</a></li>
+            <li><a href="#">SERVICE</a></li>
+            <li><a href="#">CONTACT</a></li>
+            <li><a href="#">CONTACT</a></li>
+        </ul>
+    </nav>
 </template>
   
 <script lang="ts">
 import { defineComponent } from 'vue'
+import Header from "./components/Header.vue";
 
 export default defineComponent({
-    name: 'App',
-    setup() {
-        return {
-            state: {
-                message: "SAMPLE 株式会社"
-            }
-        }
+    components: {
+        Header
     }
 })
 </script>
-  
-<style scoped>
-p {
-    font-size: 2em;
-    text-align: center;
-}
-</style>
---
+++ app/javascript/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+    <header>
+        <h1 id="logo"><a href="#">{{ campany }}</a></h1>
+    </header>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            campany: "SAMPLE 株式会社"
+        }
+    }
+}
+</script>
