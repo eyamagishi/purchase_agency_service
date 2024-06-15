d5227b8615867166d8626b66d27225d6ee7f03ed
eyamagishi <e.yamagishi0625@gmail.com>
2024-06-15 23:15:58 +0900

    add: img定義

--- app/javascript/App.vue
+++ app/javascript/App.vue
@@ -9,6 +9,39 @@
             <li><a href="#">CONTACT</a></li>
         </ul>
     </nav>
+    <div class="mainimg">
+        <div><img src="../assets/images/1.jpg" alt="" ></div>
+        <div><img src="../assets/images/2.jpg" alt="" ></div>
+        <div><img src="../assets/images/3.jpg" alt="" ></div>
+    </div>
+    <main>
+        <section>
+            <h2>お知らせ</h2>
+            <dl id="new">
+                <dt>2021/12/11</dt>
+                <dd>tp_beginner7配布開始。</dd>
+                <dt>20XX/00/00</dt>
+                <dd>サンプルテキスト。サンプルテキスト。サンプルテキスト。</dd>
+                <dt>20XX/00/00</dt>
+                <dd>サンプルテキスト。サンプルテキスト。サンプルテキスト。</dd>
+                <dt>20XX/00/00</dt>
+                <dd>サンプルテキスト。サンプルテキスト。サンプルテキスト。</dd>
+                <dt>20XX/00/00</dt>
+                <dd>サンプルテキスト。サンプルテキスト。サンプルテキスト。サンプルテキスト。サンプルテキスト。サンプルテキスト。サンプルテキスト。サンプルテキスト。サンプルテキスト。サンプルテキスト。サンプルテキスト。サンプルテキスト。
+                </dd>
+            </dl>
+        </section>
+    </main>
+    <footer>
+        <ul class="icon">
+            <li><a href="#"><img src="../assets/images/icon_facebook.png" alt="Facebook"></a></li>
+            <li><a href="#"><img src="../assets/images/icon_twitter.png" alt="Twitter"></a></li>
+            <li><a href="#"><img src="../assets/images/icon_instagram.png" alt="Instagram"></a></li>
+            <li><a href="#"><img src="../assets/images/icon_youtube.png" alt="YouTube"></a></li>
+            <li><a href="#"><img src="../assets/images/icon_line.png" alt="LINE"></a></li>
+        </ul>
+        <small>Copyright&copy; <a href="#">Sample Company</a> All Rights Reserved.</small>
+    </footer>
 </template>
   
 <script lang="ts">
